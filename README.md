3ec5d7f6f60b901d7d7a5e0b237a50044c1505f8
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-12-13 20:07:55 +0100

    Add done script

---
+++ README.md
@@ -0,0 +1,2 @@
+# zadanie-11.5 - programowanie obiektowe this. self. itp
+https://gelu4.github.io/zadanie-11.5/
---
+++ script.js
@@ -0,0 +1,24 @@
+$(function() {
+	
+function Button(text) {
+	this.text = text || "Hello";
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $("<button>");
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$("body").append(this.$element);
+	
+	}
+};
+var btn1 = new Button("Hello!");
+btn1.create();
+var btn2 = new Button("Pick My");
+btn2.create();	
+
+});
